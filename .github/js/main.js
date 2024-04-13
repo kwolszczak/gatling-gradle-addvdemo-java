a0893e49dd4cfa41f2b2098803a925bdf24c1bb7
Kamil Wolszczak <kwolszczak@sii.pl>
2024-04-13 18:11:17 +0200

    GitHub Actions - containers added custom -> JS Action [skip ci]

--- .github/js/main.js
+++ .github/js/main.js
@@ -4,6 +4,7 @@ const exec = require('@actions/exec');
 
 // run first in this directory:
 //npm install @actions/core @actions/github @actions/exec
+// add folders to git, they should be uploaded !!!
 
 function run() {
   core.notice('Hello from my custom action')
@@ -15,8 +16,12 @@ function run() {
 
   // 2) upload file
   // on linux runner there is preinstalled aws CLI to run commands
+  // zwroc uwage na cudzyslow ` pochylony
   const s3Uri = `s3://${bucket}`
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
+  const websiteUrl = `http://${bucket}.s3-website-${bucketREgion}.amazonaws.com`;
+  core.setOutput('websiteUrl', websiteUrl);
 }
 
 run()
